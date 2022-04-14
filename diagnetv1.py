a20de2801c0fe10b8af401f8f55739a4089404bc
Blaise Papa <41106147+Blvisse@users.noreply.github.com>
2022-04-15 00:38:18 +0300

    Update diagnetv1.py

--- diagnetv1.py
+++ diagnetv1.py
@@ -188,7 +188,7 @@ def predict_base64_image(name,contents):
     
     
     os.remove(file_path)
-    return {name:prediction_image,'confidence':prediction} 
+    return {name:prediction_image,'confidence':prediction.tolist()} 
 
 if __name__ == "__main__":
 
